c30e11a1f1f4f9c6cc3b3a85c86cde2b3f31ab41
Ionut <i.balai_97@yahoo.com>
2019-06-23 13:08:52 +0300

    commit 3

--- src/main/java/com/sda/playermanager/App.java
+++ src/main/java/com/sda/playermanager/App.java
@@ -1,13 +1,49 @@
 package com.sda.playermanager;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+
+	private static final Logger log = LogManager.getLogger(App.class);
+	private static final String url = "jdbc:mysql://localhost:3306/players";
+	private static final String user = "root";
+	private static final String pass = "root";
+
+	public static void main(String[] args){
+		if (log.isDebugEnabled()) {
+			log.info("main(): IN:");
+		}
+		System.out.println("Hello World!");
+
+		App app = new App();
+		app.parseAllPlayers();
+
+		if (log.isDebugEnabled()) {
+			log.info("main(): OUT:");
+		}
+	}
+
+	public void parseAllPlayers() {
+		if (log.isDebugEnabled()) {
+			log.debug("parseAllPlayers():IN:");
+		}
+		try {
+			Connection con = DriverManager.getConnection(url, user, pass);
+			Statement stm = con.createStatement();
+			ResultSet rs = stm.executeQuery("select 1");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		if (log.isDebugEnabled()) {
+			log.debug("parseAllPlayers():OUT:");
+		}
+	}
 }
